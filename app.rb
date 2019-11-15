93ecbe9269aadd57df2d246c56701aaaed910499
Ronald Stephens <rstephens@bluemorpho.cc>
2019-11-15 18:29:02 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,7 +10,7 @@ class App < Sinatra::Base
     erb :create_puppy
   end
 
-  post 'create_puppy' do
+  post '/create_puppy' do
     @puppy = Puppy.new(params[:name], params[:breed], params[:months_old])
     erb :display_puppy
   end
