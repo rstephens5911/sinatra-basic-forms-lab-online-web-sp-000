66f66a5c7fff3ab3059a22b18ec1140b81c87e72
Ronald Stephens <rstephens@bluemorpho.cc>
2019-11-15 17:53:48 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,4 @@
-require_relative 'config/environment'
+require_relative 'models/puppy'
 
 class App < Sinatra::Base
 
@@ -6,13 +6,6 @@ class App < Sinatra::Base
     erb :index
   end
 
-  get '/new' do
-    erb :create_puppy
-  end
-
-  post '/puppy' do
-    @puppy = Puppy.new(params['name'], params['breed'], params['months_old'])
-    erb :display_puppy
-  end
 
 end
+  
