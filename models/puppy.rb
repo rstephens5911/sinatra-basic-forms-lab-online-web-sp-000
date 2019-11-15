86f3e41edb2b23f9217fefbb341a9e52368d4c0f
Ronald Stephens <rstephens@bluemorpho.cc>
2019-11-15 17:56:19 +0000

    Automatically backed up by Learn

---
+++ models/puppy.rb
@@ -0,0 +1,15 @@
+class Puppy 
+  attr_accessor :name, :breed, :months_old
+
+  DOGS = []
+
+  def initialize
+    @name = name
+    @breed = breed
+    @months_old = months_old
+    DOGS << self
+  end
+
+
+end
+    
